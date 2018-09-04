a7f52ae16f8d9d22382834401c8baab0d47b2f93
Isabelle Delmas <isabelle@delmas.us>
2018-09-04 16:31:52 -0700

    score calculated and displayed

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+    private const double BOX_HEIGHT_CORRECTION = 0.15;
+    public Text currentScoreGT;
+    // Use this for initialization
+    void Start () {
+        // Initializes the score to 0
+        GameObject currentScoreGO = GameObject.Find("ScoreDisplay");
+        currentScoreGT = currentScoreGO.GetComponent<Text>();
+        currentScoreGT.text = "Score: 0 m";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        double height = this.FindHight();
+        // Update score if new maximum height was found
+        if (height > double.Parse(currentScoreGT.text))
+        {
+            currentScoreGT.text = "Score: " + height.ToString() + " m";
+        }
+	}
+
+    private double FindHight()
+    {
+        double maxheight = 0.0;
+        // Get a list of all the boxes
+        GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
+        
+        // Goes through the list to find the highest
+        foreach(GameObject box in boxes)
+        {
+            double height = box.transform.position.y - BOX_HEIGHT_CORRECTION;
+            if(height > maxheight)
+            {
+                maxheight = height;
+            }
+        }
+
+        // Return max height
+        return maxheight;
+    }
+}
