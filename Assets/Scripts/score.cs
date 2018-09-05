e1fab4edf42f49b6bda257943afbf9503c912c46
Isabelle Delmas <isabelle.lenertz@gmail.com>
2018-09-04 17:15:09 -0700

    decimal display fixed

--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -17,24 +17,24 @@ public class score : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        double height = this.FindHight();
+        decimal height = this.FindHight();
         // Update score if new maximum height was found
-        if (height > double.Parse(currentScoreGT.text))
+        if (height > decimal.Parse(currentScoreGT.text))
         {
-            currentScoreGT.text = height.ToString();
+            currentScoreGT.text = height.ToString("N2");
         }
 	}
 
-    private double FindHight()
+    private decimal FindHight()
     {
-        double maxheight = 0.0;
+        decimal maxheight = 0.0m;
         // Get a list of all the boxes
         GameObject[] boxes = GameObject.FindGameObjectsWithTag("Box");
         
         // Goes through the list to find the highest
         foreach(GameObject box in boxes)
         {
-            double height = box.transform.position.y - BOX_HEIGHT_CORRECTION;
+            decimal height = (decimal)(box.transform.position.y - BOX_HEIGHT_CORRECTION);
             if(height > maxheight)
             {
                 maxheight = height;
